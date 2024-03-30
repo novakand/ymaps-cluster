6f20385833a449680f894e343b9dd6219a327214
novakand <a79264799999@gmail.com>
2024-03-30 13:24:50 +0300

    fix

--- js/maps.js
+++ js/maps.js
@@ -5,26 +5,45 @@ import { MapConfig as config } from '../js/modules/maps/constants/map-config.con
 
 let map;
 let mapService;
+let objectManager;
 
 async function onInit() {
     onPreloader(false)
     onInitMap();
 }
 
-async function onInitMap() {
+ function onInitMap() {
     const isMobile = getDeviceMobile();
     const mapOptions = { state, options: { ...(isMobile ? { balloonPanelMaxMapArea: Infinity, ...options } : options) }, config };
     mapService = new YaMapService('map', mapOptions);
-    mapService.ready.then((yaMap) => {
+    mapService.ready.then(async (yaMap) => {
         map = yaMap;
         document.querySelector('#map').setAttribute('data-load', true);
         onPreloader(true);
+        onInitobjectManager();
+        map.geoObjects.add(objectManager);
+        getData().then((data)=>{
+            objectManager.add(data);
+            fitBounds();
+            onPreloader(false);
+        });
+       
     });
+}
+
+function onInitobjectManager() {
+    objectManager = new ymaps.ObjectManager({
+        clusterize: true,
+        gridSize: 160,
+        clusterDisableClickZoom: true
+    });
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
 }
 
 function onPreloader(isShow) {
     const preloader = document.querySelector('.mdc-linear-progress');
-    delay(3000).then(() => isShow ? preloader.style.width = '100%' : preloader.style.width = '0');
+    delay(1000).then(() => isShow ? preloader.style.width = '100%' : preloader.style.width = '0');
 }
 
 function delay(ms) {
@@ -33,6 +52,22 @@ function delay(ms) {
 
 function getDeviceMobile() {
     return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+}
+
+async function getData() {
+    const response = await fetch('./data-storage/data.json');
+    return await response.json();
+}
+
+function fitBounds() {
+    const options = {
+        checkZoomRange: false,
+        useMapMargin: true,
+        duration: 180,
+    };
+
+    map.setBounds(map.geoObjects.getBounds(), options);
+
 }
 
 
